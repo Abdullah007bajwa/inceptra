0e695d235d8bdcbeb9c1771bfa57d2dd97dead14
Abdullah007bajwa <bajwa15523@gmail.com>
2025-08-03 17:03:31 +0500

    Untrack server directory so we can add it as a submodule

--- server/src/utils/db.ts
+++
@@ -1,6 +0,0 @@
-// src/utils/db.ts
-import prismaPkg from '@prisma/client';
-
-const PrismaClient = (prismaPkg as typeof import('@prisma/client')).PrismaClient;
-
-export const prisma = new PrismaClient();
